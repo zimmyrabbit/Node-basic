6a917a0b7a5a49abc47cbc2d51938e703841772f
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-07-27 23:23:35 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://zimmyrabbit:4219@node-basic.dgxyg.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser : true,
+  useUnifiedTopology : true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
