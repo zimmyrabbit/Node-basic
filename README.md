8c135c6ed5579ae8496f18302b90998c4d922431
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-08-11 23:32:59 +0900

    React Router DOM 설정

--- README.md
+++ README.md
@@ -35,3 +35,6 @@ npm install winston --save
 
 --React 설치
 npx create-react-app .
+
+--React Router DOM 설치
+npm install react-router-dom
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  //Link
+  } from "react-router-dom";
+  
+  import LandingPage from './components/views/LandingPage/LandingPage'
+  import LoginPage from './components/views/LoginPage/LoginPage'
+  import RegisterPage from './components/views/RegisterPage/RegisterPage'
+  
+  function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/" element = {<LandingPage/>}/>
+          <Route exact path="/login" element = {<LoginPage/>}/>
+          <Route exact path="/register" element = {<RegisterPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+  
+  }
+
+export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
