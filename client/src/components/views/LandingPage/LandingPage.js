5399f8085b9332dc4d8d55d65f22e5d7e603b59d
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-08-17 00:12:29 +0900

    로그인 페이지 기능 완성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,7 +9,10 @@ function LandingPage() {
   }, [])
 
   return (
-    <div>LandingPage</div>
+    <div style={{display : 'flex', justifyContent : 'center', alignItems : 'center'
+                , width : '100%', height : '100vh'}}>
+      <h2>LandingPage</h2>
+    </div>
   )
 }
 
